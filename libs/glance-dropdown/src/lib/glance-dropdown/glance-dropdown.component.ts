db620d149c3ba5e2fb04c1561977c7a5f4673ee2
hugoBuissez <hugobuissez@gmail.com>
2024-12-21 16:39:16 +0100

    chore(dropdown): popper dependency

--- libs/glance-dropdown/src/lib/glance-dropdown/glance-dropdown.component.ts
+++ libs/glance-dropdown/src/lib/glance-dropdown/glance-dropdown.component.ts
@@ -1,16 +1,13 @@
 import {
   Component,
-  contentChild,
   ElementRef,
   EventEmitter,
   input,
+  OnInit,
   Output,
-  signal,
-  TemplateRef,
-  viewChild,
   ViewChild,
 } from '@angular/core';
-import { CommonModule, NgTemplateOutlet } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import {
   Placement,
   computePosition,
@@ -18,9 +15,11 @@ import {
   flip,
   shift,
   autoUpdate,
+  arrow,
 } from '@floating-ui/dom';
 import { animate } from 'motion';
 import { fromEvent } from 'rxjs';
+import { GlancePopperComponent } from '@glance-ui/popper';
 
 export type GDropdownItem = {
   label: string;
@@ -35,97 +34,14 @@ export type GDropdownItem = {
 @Component({
   selector: 'g-dropdown',
   standalone: true,
-  imports: [CommonModule, NgTemplateOutlet],
+  imports: [CommonModule],
   templateUrl: './glance-dropdown.component.html',
 })
-export class GDropdownComponent {
-  @ViewChild('toggle') toggle!: ElementRef;
-  @ViewChild('popper') popper!: ElementRef;
-
-  @Output() onShow = new EventEmitter<void>();
-  @Output() onHide = new EventEmitter<void>();
-
+export class GDropdownComponent extends GlancePopperComponent {
   items = input<GDropdownItem[][]>([]);
-  mode = input<'hover' | 'click'>('click');
-  placement = input<Placement>('bottom-start');
-
-  private cleanup: (() => void) | undefined;
-
-  ngOnInit() {
-    if (this.mode() === 'click') {
-      // Hide popper when click outside the toggle
-      fromEvent(document, 'click').subscribe((event) => {
-        if (!this.toggle.nativeElement.contains(event.target as Node)) {
-          this.hidePopper();
-        }
-      });
-
-      // or when ESC key is pressed
-      fromEvent(document, 'keydown').subscribe((event: Event) => {
-        if ((event as KeyboardEvent).key === 'Escape') {
-          this.hidePopper();
-        }
-      });
-    }
-  }
 
   protected onItemClick(item: GDropdownItem) {
     if (item.disabled) return;
     if (item.onClick) item.onClick();
-  }
-
-  protected update() {
-    computePosition(this.toggle.nativeElement, this.popper.nativeElement, {
-      placement: this.placement(),
-      middleware: [offset(6), flip(), shift({ padding: 5 })],
-    }).then(({ x, y }) => {
-      Object.assign(this.popper.nativeElement.style, {
-        left: `${x}px`,
-        top: `${y}px`,
-      });
-    });
-  }
-
-  protected onMouseEnter() {
-    if (this.mode() === 'hover') this.showPopper();
-  }
-
-  protected onMouseLeave() {
-    if (this.mode() === 'hover') this.hidePopper();
-  }
-
-  protected onToggleClick() {
-    if (this.mode() === 'click') {
-      // if popper is already shown, hide it
-      if (this.popper.nativeElement.style.display === 'block') {
-        this.hidePopper();
-      } else {
-        this.showPopper();
-      }
-    }
-  }
-
-  protected showPopper() {
-    this.onShow.emit();
-    animate(
-      this.popper.nativeElement,
-      { display: 'block', scale: [0.8, 1], opacity: 1 },
-      { duration: 0.1 }
-    );
-    this.cleanup = autoUpdate(
-      this.toggle.nativeElement,
-      this.popper.nativeElement,
-      this.update.bind(this)
-    );
-  }
-
-  protected hidePopper() {
-    this.onHide.emit();
-    animate(
-      this.popper.nativeElement,
-      { display: 'none', opacity: 0 },
-      { duration: 0.1 }
-    );
-    this.cleanup?.();
   }
 }
