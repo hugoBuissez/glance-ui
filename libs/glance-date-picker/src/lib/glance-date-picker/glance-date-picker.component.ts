8336a46e81803dfcdfc4a17f8a54dea60612c83e
hugoBuissez <hugobuissez@gmail.com>
2024-12-23 16:43:29 +0100

    feat(dp): v2 range dp

--- libs/glance-date-picker/src/lib/glance-date-picker/glance-date-picker.component.ts
+++ libs/glance-date-picker/src/lib/glance-date-picker/glance-date-picker.component.ts
@@ -6,13 +6,14 @@ import {
   output,
   signal,
 } from '@angular/core';
-import { NgClass, NgStyle } from '@angular/common';
+import { NgClass, NgStyle, NgTemplateOutlet } from '@angular/common';
 import { GlancePopperComponent } from '@glance-ui/popper';
 import {
   addDays,
   addMonths,
   isAfter,
   isBefore,
+  isSameDay,
   isSameMonth,
   startOfMonth,
   startOfWeek,
@@ -27,12 +28,13 @@ interface DatePickerDay {
   isEndDate: boolean;
   isInCurrentMonth: boolean;
   isDisabled?: boolean;
+  isHovered?: boolean;
 }
 
 @Component({
   selector: 'g-date-picker',
   standalone: true,
-  imports: [NgStyle, NgClass],
+  imports: [NgStyle, NgClass, NgTemplateOutlet],
   templateUrl: './glance-date-picker.component.html',
 })
 export class GlanceDatePickerComponent
@@ -42,39 +44,84 @@ export class GlanceDatePickerComponent
   readonly WEEK_DAYS = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
 
   disableFrom = input<Date | null>(null);
+  double = input<boolean>(true);
 
-  private currentDate = signal<Date>(startOfMonth(new Date()));
+  private currentMonthDate = signal<Date>(startOfMonth(new Date()));
+  private nextMonthDate = computed(() =>
+    startOfMonth(addMonths(this.currentMonthDate(), 1))
+  );
 
   selectedStartDate = signal<Date | null>(null);
   selectedEndDate = signal<Date | null>(null);
 
-  currentWeeks = signal<DatePickerDay[][]>(
-    this.updateCalendarDays(this.currentDate())
-  );
-  nextMonthWeeks = signal<DatePickerDay[][]>(
-    // this.generateCalendarDays(addMonths(this.currentDate(), 1))
-    []
-  );
+  currentWeeks = computed(() => {
+    const weeks: DatePickerDay[][] = [];
+    for (let month = 0; month < (this.double() ? 2 : 1); month++) {
+      const firstDayOfMonth = startOfMonth(
+        addMonths(this.currentMonthDate(), month)
+      );
+      const firstDayOfWeek = startOfWeek(firstDayOfMonth, {
+        weekStartsOn: 1,
+      });
+
+      // Generate 6 weeks to ensure we have enough rows
+      for (let week = 0; week < 6; week++) {
+        const weekDates: DatePickerDay[] = [];
+
+        // Generate 7 days for each week
+        for (let day = 0; day < 7; day++) {
+          const date = addDays(firstDayOfWeek, week * 7 + day);
+          weekDates.push({
+            date,
+            isInRange: this.isDateInRange(date, this.hoveredDate()),
+            isStartDate: this.isDateStartDate(date),
+            isEndDate: this.isDateEndDate(date),
+            isInCurrentMonth: isSameMonth(date, firstDayOfMonth),
+            isDisabled:
+              !!this.disableFrom() && isAfter(date, this.disableFrom()!),
+            isHovered:
+              !!this.hoveredDate() && isSameDay(date, this.hoveredDate()!),
+          });
+        }
+
+        weeks.push(weekDates);
+      }
+    }
+
+    return weeks;
+  });
+
+  leftMonth = computed(() => {
+    // take first 6 weeks of current month
+    return this.currentWeeks()
+      .slice(0, 6)
+      .filter((week) => week.some((day) => day.isInCurrentMonth));
+  });
+
+  rightMonth = computed(() => {
+    // take first 6 weeks of next month
+    return this.currentWeeks()
+      .slice(6, 12)
+      .filter((week) => week.some((day) => day.isInCurrentMonth));
+  });
 
   formattedMonthLeft = computed(() => {
-    const fmt = this.currentDate().toLocaleString('default', {
+    const fmt = this.currentMonthDate().toLocaleString('default', {
       month: 'long',
     });
     return fmt.charAt(0).toUpperCase() + fmt.slice(1);
   });
 
-  formattedYearLeft = computed(() => this.currentDate().getFullYear());
+  formattedYearLeft = computed(() => this.currentMonthDate().getFullYear());
 
   formattedMonthRight = computed(() => {
-    const fmt = addMonths(this.currentDate(), 1).toLocaleString('default', {
+    const fmt = this.nextMonthDate().toLocaleString('default', {
       month: 'long',
     });
     return fmt.charAt(0).toUpperCase() + fmt.slice(1);
   });
 
-  formattedYearRight = computed(() =>
-    addMonths(this.currentDate(), 1).getFullYear()
-  );
+  formattedYearRight = computed(() => this.nextMonthDate().getFullYear());
 
   hoveredDate = signal<Date | null>(null);
 
@@ -111,105 +158,43 @@ export class GlanceDatePickerComponent
     }
 
     // TODO: refactor this to use a single method
-    this.currentWeeks.update(() =>
-      this.currentWeeks().map((week) =>
-        week.map((day) => ({
-          ...day,
-          isInRange: this.isDateInRange(day.date),
-          isStartDate: this.isDateStartDate(day.date),
-          isEndDate: this.isDateEndDate(day.date),
-        }))
-      )
-    );
-    this.nextMonthWeeks.update(() =>
-      this.nextMonthWeeks().map((week) =>
-        week.map((day) => ({
-          ...day,
-          isInRange: this.isDateInRange(day.date),
-          isStartDate: this.isDateStartDate(day.date),
-          isEndDate: this.isDateEndDate(day.date),
-        }))
-      )
-    );
+    // this.currentWeeks.update(() =>
+    //   this.currentWeeks().map((week) =>
+    //     week.map((day) => ({
+    //       ...day,
+    //       isInRange: this.isDateInRange(day.date),
+    //       isStartDate: this.isDateStartDate(day.date),
+    //       isEndDate: this.isDateEndDate(day.date),
+    //     }))
+    //   )
+    // );
+    // this.nextMonthWeeks.update(() =>
+    //   this.nextMonthWeeks().map((week) =>
+    //     week.map((day) => ({
+    //       ...day,
+    //       isInRange: this.isDateInRange(day.date),
+    //       isStartDate: this.isDateStartDate(day.date),
+    //       isEndDate: this.isDateEndDate(day.date),
+    //     }))
+    //   )
+    // );
   }
 
-  // hover effect when user has selected start date
-  // it changes the background color of the days between start and current hovered date
-  // the method handles the case when hovered date is before start date
   onCellHover(date: Date): void {
     if (!this.selectedStartDate() || this.selectedEndDate()) return;
     this.hoveredDate.set(date);
-
-    // TODO: refactor this to use a single method
-    this.currentWeeks.update((weeks) =>
-      weeks.map((week) =>
-        week.map((day) => ({
-          ...day,
-          isInRange:
-            !this.isDateSelected(day.date) &&
-            day.date !== this.hoveredDate() &&
-            ((day.date >= this.selectedStartDate()! && day.date <= date) ||
-              (day.date <= this.selectedStartDate()! && day.date >= date)),
-        }))
-      )
-    );
-    this.nextMonthWeeks.update((weeks) =>
-      weeks.map((week) =>
-        week.map((day) => ({
-          ...day,
-          isInRange:
-            !this.isDateSelected(day.date) &&
-            day.date !== this.hoveredDate() &&
-            ((day.date >= this.selectedStartDate()! && day.date <= date) ||
-              (day.date <= this.selectedStartDate()! && day.date >= date)),
-        }))
-      )
-    );
   }
 
-  private updateCalendarDays(date: Date): DatePickerDay[][] {
-    const weeks: Date[][] = [];
-    const firstDayOfWeek = startOfWeek(date, { weekStartsOn: 1 });
-
-    // Generate 6 weeks to ensure we have enough rows
-    for (let week = 0; week < 6; week++) {
-      const weekDates: Date[] = [];
-
-      // if week start date is not in current month, skip it (except for the first week)
-      const startDate = addDays(firstDayOfWeek, week * 7);
-      if (!isSameMonth(startDate, date) && week !== 0) {
-        continue;
-      }
-
-      // Generate 7 days for each week
-      for (let day = 0; day < 7; day++) {
-        const date = addDays(firstDayOfWeek, week * 7 + day);
-        weekDates.push(date);
-      }
-      weeks.push(weekDates);
-    }
-
-    // Only return the weeks that contain days in the current month
-    return weeks.map((week) =>
-      week.map(
-        (d) =>
-          ({
-            date: d,
-            isInRange: this.isDateInRange(d),
-            isStartDate: this.isDateStartDate(d),
-            isEndDate: this.isDateEndDate(d),
-            isInCurrentMonth: isSameMonth(d, date),
-            isDisabled: this.disableFrom() && isAfter(d, this.disableFrom()!),
-          } as DatePickerDay)
-      )
-    );
-  }
-
-  private isDateInRange(date: Date): boolean {
+  // checks if date is in range between start and end date
+  // if hovered date is provided, it checks if date is in range between start and hovered date
+  private isDateInRange(date: Date, hoveredDate: Date | null): boolean {
     const start = this.selectedStartDate();
-    const end = this.selectedEndDate();
-    if (!start || !end || this.isDateSelected(date)) return false;
-    return date >= start && date <= end;
+    const end = this.selectedEndDate() || hoveredDate;
+
+    if (!start || !end) return false;
+    if (isSameDay(date, end) || isSameDay(date, start)) return false;
+    if (isAfter(date, start) && isBefore(date, end)) return true;
+    return !!hoveredDate && isBefore(date, start) && isAfter(date, hoveredDate);
   }
 
   private isDateStartDate(date: Date): boolean {
@@ -226,18 +211,10 @@ export class GlanceDatePickerComponent
 
   // Navigation methods
   nextMonth(): void {
-    this.currentDate.update((date) => addMonths(date, 1));
-    this.currentWeeks.update(() => this.updateCalendarDays(this.currentDate()));
-    this.nextMonthWeeks.update(() =>
-      this.updateCalendarDays(addMonths(this.currentDate(), 1))
-    );
+    this.currentMonthDate.update((date) => addMonths(date, 1));
   }
 
   previousMonth(): void {
-    this.currentDate.update((date) => subMonths(date, 1));
-    this.currentWeeks.update(() => this.updateCalendarDays(this.currentDate()));
-    this.nextMonthWeeks.update(() =>
-      this.updateCalendarDays(subMonths(this.currentDate(), 1))
-    );
+    this.currentMonthDate.update((date) => subMonths(date, 1));
   }
 }
